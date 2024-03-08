a67a5b45a3e3d4f207f1e58ce211fb9c44883ecb
isabellebryans <bryansi@tcd.ie>
2024-03-08 11:31:34 +0000

    File upload works

--- src/main/java/Server/EthicalLODCheckerController.java
+++ src/main/java/Server/EthicalLODCheckerController.java
@@ -3,8 +3,12 @@ import Checker.Dataset;
 import Utilities.LoadModel;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.riot.Lang;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
@@ -24,6 +28,22 @@ public class EthicalLODCheckerController {
             Dataset dataSet = new Dataset(model1);
 
             return "Done";
+        }
+
+        @PostMapping("/upload")
+        public ResponseEntity<String> handleFileUpload(@RequestParam("file") MultipartFile file) {
+            // Process the file
+            try {
+                // Logic to process the file
+                System.out.println("Original filename: " + file.getOriginalFilename());
+                System.out.println("File size: " + file.getSize());
+
+                // Place your file processing logic here
+
+                return ResponseEntity.ok("File uploaded successfully: " + file.getOriginalFilename());
+            } catch (Exception e) {
+                return ResponseEntity.badRequest().body("Could not upload the file: " + e.getMessage());
+            }
         }
 
 

d1055659b8bfb7fd5c4b5fa5168daacb0b1670de
isabellebryans <bryansi@tcd.ie>
2024-03-08 17:47:30 +0000

    Added CORS so can connect to front end

--- src/main/java/Server/EthicalLODCheckerController.java
+++ src/main/java/Server/EthicalLODCheckerController.java
@@ -2,24 +2,24 @@ package Server;
 import Checker.Dataset;
 import Utilities.DownloadFile;
 import Utilities.LoadModel;
+import org.apache.jena.base.Sys;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.riot.Lang;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-
+@CrossOrigin
 @RestController
 public class EthicalLODCheckerController {
         @GetMapping("/hello")
-        public String sayHello() {
-            return "Hello, World!";
+        public ResponseEntity<String> sayHello() {
+            System.out.println("Hello!");
+            return ResponseEntity.ok("Hello world!");
         }
         @GetMapping("/runCamdenEg")
         public String camdenEg() throws IOException {
@@ -34,7 +34,7 @@ public class EthicalLODCheckerController {
         }
 
         @PostMapping("/upload")
-        public String handleFileUpload(@RequestParam("file") MultipartFile file) {
+        public ResponseEntity<String> handleFileUpload(@RequestParam("file") MultipartFile file) {
             // Process the file
             try {
                 // Create a temporary file in the system's temporary directory
@@ -46,17 +46,22 @@ public class EthicalLODCheckerController {
                 // Process the file
                 Model m = LoadModel.initAndLoadModelFromFolder(tempFile.toFile(), Lang.RDFXML);
                 if (m==null){
-                    return "Can't load model from the file, incorrect RDF";
+                    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to get model from file");
+
                 }
                 Dataset dataset = new Dataset(m);
                 // After processing, if you no longer need the file, consider deleting it
                 Files.delete(tempFile);
-
+                System.out.println("Here");
                 // Return a success response
-                return "File uploaded successfully: " + file.getOriginalFilename() + " at " + tempFile;
+                //return ResponseEntity.ok("File uploaded successfully: " + file.getOriginalFilename() + " at " + tempFile);
+                ResponseEntity<String> res = ResponseEntity.ok().build();
+                System.out.println(res.toString());
+                return ResponseEntity.ok().build();
             } catch (IOException e) {
                 e.printStackTrace();
-                return "Failed to upload file: " + e.getMessage();
+                // Return an error response with a 500 Internal Server Error status
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to upload file: " + e.getMessage());
             }
         }
 

e8bf18ed050cff5e8a71a6b6d7862315d49f1b02
isabellebryans <bryansi@tcd.ie>
2024-03-08 10:18:19 +0000

    Init

---
+++ src/main/java/ie/tcd/EthicalLODCheckerServer/EthicalLodCheckerServerApplication.java
@@ -0,0 +1,13 @@
+package ie.tcd.EthicalLODCheckerServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EthicalLodCheckerServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EthicalLodCheckerServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ie/tcd/EthicalLODCheckerServer/EthicalLodCheckerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.tcd.EthicalLODCheckerServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EthicalLodCheckerServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
